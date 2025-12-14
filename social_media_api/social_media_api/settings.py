177bdb28665426a83a67b8f08ae260c4aea30d3d
nobbonsu <nobbonsu440@gmail.com>
2025-12-14 20:50:21 +0000

    delopyment update

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -95,8 +95,12 @@ SIMPLE_JWT = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EYECARECONNECT',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Nobbonsu617$',
+        'PORT': 'port'
     }
 }
 
